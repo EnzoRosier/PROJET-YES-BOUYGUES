12c9da92816261812b19e89f6676af63ef06c867
Olivier P <90192573+olivierpapeghin@users.noreply.github.com>
2025-12-18 14:34:43 +0100

    Ebauche du truc mais en attente du login

--- front/src/admin-tickets/admin-tickets.tsx
+++ front/src/admin-tickets/admin-tickets.tsx
@@ -1,15 +1,44 @@
 import './admin-tickets.css';
-import { Routes, Route } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 
-export default function AdminTickets() {
+export default async function AdminTickets() {
+
+    const checkLoggedIn = async () => {
+        // vérifier si l'utilisateur est connecté
+        try {
+            const response = await fetch('http://localhost:3000/admins/me', {
+                method: 'GET',
+                credentials: 'include',
+            });
+            if (response.ok) {
+                const me = await response.json();
+                return me != null;
+            } else {
+                return false;
+            }
+        }
+        catch (error) {
+            console.log('Erreur lors de la vérification de la connexion');
+            return false;
+        }
+    }
+    var loggedIn = await checkLoggedIn();
+    
 
     const getAllTickets = () => {
-        
+
     }
 
-    return (
+    // Si non connecté, rediriger vers la page de login
+    if (!loggedIn) {
+        return (<Navigate to="/login" replace />);
+        }
+    // Sinon on peut afficher la page normalement
+    else {
+        return(
         <div className="admin-tickets">
             <h2>Page Admin Tickets</h2>
         </div>
-    );
+        );
+    }
 }
