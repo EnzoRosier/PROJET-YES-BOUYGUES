a4b8d4535838b7817f35d40cd70746d79a1d00b1
Olivier P <90192573+olivierpapeghin@users.noreply.github.com>
2025-12-18 13:19:36 +0100

    Création des pages

---
+++ front/src/admin-tickets/admin-tickets.tsx
@@ -0,0 +1,15 @@
+import './admin-tickets.css';
+import { Routes, Route } from 'react-router-dom';
+
+export default function AdminTickets() {
+
+    const getAllTickets = () => {
+        
+    }
+
+    return (
+        <div className="admin-tickets">
+            <h2>Page Admin Tickets</h2>
+        </div>
+    );
+}
