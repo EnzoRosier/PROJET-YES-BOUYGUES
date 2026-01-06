9c7112d9254e2fd0656fceb2c23aebbdd4153ac1
paulD-isekai <paul.delerue@student.junia.com>
2026-01-06 09:30:24 +0100

    List

---
+++ front/src/Gestion-admin/SuperAdminList.tsx
@@ -0,0 +1,49 @@
+import './SuperAdminList.css';
+import { Navigate } from 'react-router-dom';
+import { useState } from 'react';
+
+export default function AdminList() {
+
+    const [loggedIn, setLoggedIn] = useState<boolean | null>(null);
+
+    const checkLoggedIn = async () => {
+        try {
+            const response = await fetch('http://localhost:3001/admins/me', {
+                method: 'GET',
+                credentials: 'include',
+            });
+            if (response.ok) {
+                const me = await response.json();
+                if(me != null){
+                    setLoggedIn(true);
+                }
+            } else {
+                setLoggedIn(false);
+            }
+        }
+        catch (error) {
+            console.log('Erreur lors de la vérification de la connexion');
+            setLoggedIn(false);
+        }
+    }
+    
+    checkLoggedIn();
+
+ // Si en cours d'évaluation, on met le chargement.
+    if (loggedIn === null) {
+        return <div className="admin-tickets"><h2>Chargement...</h2></div>;
+    }
+    // Si pas connecté, on redirige vers la page de login.
+    if (!loggedIn) {
+        console.log("Utilisateur non connecté, redirection vers login");
+        return (<Navigate to="/login" replace />);
+        }
+    // Sinon on peut afficher la page normalement.
+    else {
+        return(
+        <div className="admin-tickets">
+            <h2>Page Admin Tickets</h2>
+        </div>
+        );
+    }
+}
